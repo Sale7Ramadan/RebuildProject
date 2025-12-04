d8eef18a2f4bccb03cb324de6f664eb98c016f0c
Saleh Ramadan <salehramadan77711@gmail.com>
2025-12-04 22:31:55 +0300

    RepairMistakes

---
+++ DataAccessLayer/Migrations/20251204192904_cascadeAddedFoReportAndComment.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class cascadeAddedFoReportAndComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Reports",
+                table: "Comments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Reports",
+                table: "Comments",
+                column: "ReportID",
+                principalTable: "Reports",
+                principalColumn: "ReportID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Reports",
+                table: "Comments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Reports",
+                table: "Comments",
+                column: "ReportID",
+                principalTable: "Reports",
+                principalColumn: "ReportID");
+        }
+    }
+}
